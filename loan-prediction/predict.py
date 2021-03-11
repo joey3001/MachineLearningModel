3d27aa51633b4463df174d203ade0c826abdb64f
Joey3001 <josephnero111@gmail.com>
2021-03-10 16:03:34 -0800

    Fill in Prediction script.

--- loan-prediction/predict.py
+++ loan-prediction/predict.py
@@ -15,12 +15,26 @@ def cross_validate(train):
   return predictions  
 
 def compute_error(target, predictions):
-    return metrics.accuracy_score(target, predictions)
+  return metrics.accuracy_score(target, predictions)
 
 def compute_false_negatives(target, predictions):
-    df = pd.DataFrame({"target": target, "predictions": predictions})
-    return df[(df["target"] == 1) & (df["predictions"] == 0)].shape[0] / (df[(df["target"] == 1)].shape[0] + 1)
+  df = pd.DataFrame({"target": target, "predictions": predictions})
+  return df[(df["target"] == 1) & (df["predictions"] == 0)].shape[0] / (df[(df["target"] == 1)].shape[0] + 1)
 
 def compute_false_positives(target, predictions):
-    df = pd.DataFrame({"target": target, "predictions": predictions})
-    return df[(df["target"] == 0) & (df["predictions"] == 1)].shape[0] / (df[(df["target"] == 0)].shape[0] + 1)
+  df = pd.DataFrame({"target": target, "predictions": predictions})
+  return df[(df["target"] == 0) & (df["predictions"] == 1)].shape[0] / (df[(df["target"] == 0)].shape[0] + 1)
+
+def read():
+  train = pd.read_csv(os.path.join(settings.PROCESSED_DIR, "train.csv"))
+  return train
+
+if __name__ == "__main__":
+  train = read()
+  predictions = cross_validate(train)
+  error = compute_error(train[settings.TARGET], predictions)
+  fn = compute_false_negatives(train[settings.TARGET], predictions)
+  fp = compute_false_positives(train[settings.TARGET], predictions)
+  print("Accuracy Score: {}".format(error))
+  print("False Negatives: {}".format(fn))
+  print("False Positives: {}".format(fp))
